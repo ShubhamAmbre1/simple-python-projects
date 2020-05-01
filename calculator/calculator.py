74e1d7a010552d18f1d22897b68e82209f7dd1f3
ShubhamAmbre1 <shubhamambre94@gmail.com>
2020-05-01 16:23:54 +0530

    calculator

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -18,17 +18,30 @@ def button_clear():
     add_list.clear()
 
 add_list = []
-
+ans = 0
 def button_add():
     add_list.append(int(e.get()))
     e.delete(0, END)
+    #e.insert(0, sum(add_list))
+    
+    global ans
+    ans = sum(add_list)
+    print(add_list)
+
+def button_sub():
+    pass
+    add_list.append(int(e.get()))
+    sub = e.get()
+    e.delete(0, END)
+    #e.insert(0, sum(add_list) - int(sub))
+    add_list.append(sum(add_list) - int(sub))
     print(add_list)
 
 def equal():
     add_list.append(int(e.get()))
     e.delete(0, END)
     print(add_list)
-    e.insert(0, sum(add_list))
+    e.insert(0, ans)
 
 #iniatialize widgets
 but0 = Button(root, text = "0", command = lambda: push(0), padx = 20, pady = 20)
@@ -41,9 +54,10 @@ but6 = Button(root, text = "6", command = lambda: push(6), padx = 20, pady = 20)
 but7 = Button(root, text = "7", command = lambda: push(7), padx = 20, pady = 20)
 but8 = Button(root, text = "8", command = lambda: push(8), padx = 20, pady = 20)
 but9 = Button(root, text = "9", command = lambda: push(9), padx = 20, pady = 20)
-but_add = Button(root, text = "ADD", command = lambda: button_add(), padx = 11, pady = 20)
-but_clear = Button(root, text = "CLEAR", command = button_clear, padx = 5.5, pady = 20)
-but_equal = Button(root, text = "=", command = equal, padx = 75, pady = 20)
+but_add = Button(root, text = "+", command = lambda: button_add(), padx = 20, pady = 20)
+but_sub = Button(root, text = "-", command = lambda: button_sub(), padx = 20, pady = 20)
+but_clear = Button(root, text = "CLEAR", command = button_clear, padx = 6, pady = 20)
+but_equal = Button(root, text = "=", command = equal, padx = 46, pady = 20)
 
 #Position Widgets
 but1.grid(row = 2, column = 1)
@@ -58,9 +72,10 @@ but7.grid(row = 4, column = 1)
 but8.grid(row = 4, column = 2)
 but9.grid(row = 4, column = 3)
 
-but0.grid(row = 5, column = 1)
-but_add.grid(row = 5, column = 2)
-but_clear.grid(row = 5, column = 3)
-but_equal.grid(row = 6,column = 1, columnspan = 3)
+but0.grid(row = 5, column = 2)
+but_add.grid(row = 5, column = 1)
+but_sub.grid(row = 5, column = 3)
+but_clear.grid(row = 6, column = 1)
+but_equal.grid(row = 6,column = 2, columnspan = 2)
 
 root.mainloop()  
